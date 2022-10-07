13cac8e7978a9438bf49b91069f6f975d8de04b2
Mubeen Ali <cmubeenali@gmail.com>
2022-10-07 16:59:25 +0530

    experimented request and response

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+import base64
 import socket
+import struct
 import sys
 
 HOST='0.0.0.0'
@@ -15,16 +17,38 @@ except socket.error as err:
 conn=None
 addr=None
 try:
-    t_sock.listen(1)
+    t_sock.listen(1)    
 
     print('LISTENING ON '+HOST+':'+str(PORT))
 
-    conn,addr= t_sock.accept()
-    print(addr[0]+':'+str(addr[1])+' CONNECTED')
-    data= conn.recv(1024)
-    conn.close()
+    i=0
+    while i<5:
+        conn,addr= t_sock.accept()
+        conn.setblocking(0)
+        
+        print(addr[0]+':'+str(addr[1])+' CONNECTED')
+        data=bytearray()
+        while True:            
+            try:
+                packet= conn.recv(1024)                
+                data.extend(packet)
+                if len(packet)<1024:
+                    break
+            except Exception as err:
+                break
+        print('DATA RECEIVED : '+data.decode('utf-8'))
+        resp="""HTTP/1.1 404 Not Found
+        Content-Type: text/html
+
+        Not Found
+        """.encode()
+        conn.send(resp)
+        conn.close()
+        i+=1
+    t_sock.close()
 except Exception as err:
     print('CONNECTION_ACCEPT_ERROR : '+str(err))
     if conn is not None:
         conn.close()
+    t_sock.close()
     sys.exit()
